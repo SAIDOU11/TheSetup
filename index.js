1050f55268974b26287b990809fbfd57997140bf
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-02 05:55:52 +0200

    2 faons différentes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function getDiceRollArray(diceCount) {
-  return new Array(diceCount).fill(0).map(function () {
-    return Math.floor(Math.random() * 6) + 1;
+  return new Array(diceCount).fill(0).map(() => {
+    // No parameters needed
+    return Math.floor(Math.random() * 6 + 1);
   });
+
+  // let newDiceRolls = [];
+  // for (let i = 0; i < diceCount; i++) {
+  //   newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+  // }
+  // return newDiceRolls;
 }
 
 function getDiceHtml(diceCount) {
