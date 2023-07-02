2ea0bb67373fecaa8b2b346b9b577aa48dd99b1b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-02 06:19:51 +0200

    function

---
+++ Aside constructor function/index.js
@@ -0,0 +1,17 @@
+const animalForRelease1 = {
+  name: "Tilly",
+  species: "tiger",
+  weightKg: 56,
+  age: 2,
+  dateOfRelease: "03-02-22",
+};
+
+const animalForRelease2 = {
+  name: "Nelly",
+  species: "elephant",
+  weightKg: 320,
+  age: 16,
+  dateOfRelease: "03-02-22",
+};
+
+function Animal(data) {}
