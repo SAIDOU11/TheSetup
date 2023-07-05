f1596b2bb7c00e9512645b8b11011fae6343bc06
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-05 18:13:19 +0200

    class

---
+++ Aside Classes/class2.js
@@ -0,0 +1,17 @@
+const moduleStats = {
+  module1: {
+    moduleName: "Learn JS",
+    studentsEnrolled: 2340,
+    studentsCompleted: 2210,
+  },
+  module2: {
+    moduleName: "CSS Basics",
+    studentsEnrolled: 1893,
+    studentsCompleted: 1810,
+  },
+  module3: {
+    moduleName: "Responsive Design",
+    studentsEnrolled: 4600,
+    studentsCompleted: 4357,
+  },
+};
