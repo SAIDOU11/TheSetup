6fc32f762744c3f0ee871a9a54ef768c8404f7aa
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-11 11:08:01 +0200

    delete log

--- Aside .reduce()/exercice.js
+++ Aside .reduce()/exercice.js
@@ -1,7 +1,6 @@
 const grades = [75, 83, 66, 43, 55, 99, 87, 16, 89, 64, 70, 80, 94, 77, 66, 73];
 
 const totalGrades = grades.reduce((total, currentGrades) => {
-  console.log("total: " + total + "currentGrades: " + currentGrades);
   return total + currentGrades;
 });
 
--- Aside .reduce()/index.js
+++ Aside .reduce()/index.js
@@ -2,7 +2,6 @@ const rainJanuaryByWeek = [10, 20, 0, 122];
 
 const totalRainfallJanuary = rainJanuaryByWeek.reduce(
   (total, currentElement) => {
-    // console.log(`total: ` + total, "currentElement: " + currentElement);
     return total + currentElement;
   }
 );
